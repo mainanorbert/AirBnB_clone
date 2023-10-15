1ae3bc376d936c8626b53333d84fffbcec5caf93
mainanorbert <mainanorbert@gmail.com>
2023-10-15 09:26:57 +0000

    podstyle compliant

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,6 +4,8 @@ Importing modules
 """
 import unittest
 from datetime import datetime
+import models
+import time
 from models.base_model import BaseModel
 
 
@@ -32,6 +34,43 @@ class Test_BaseModel(unittest.TestCase):
         obj = BaseModel()
         obj_dict = obj.to_dict()
         self.assertEqual(obj.id, obj_dict["id"])
+
+    def test_str(self):
+        obj = BaseModel()
+        obj_str = str(obj)
+        self.assertIn(obj.__class__.__name__, obj_str)
+        self.assertIn(obj.id, obj_str)
+
+    def test_Instance_stored_in_objs(self):
+        self.assertIn(BaseModel(), models.storage.all().values())
+
+    def test_id_str(self):
+        obj = BaseModel()
+        self.assertEqual(type(obj.id), str)
+
+    def test_documentation(self):
+        """ testing for for documentation """
+        self.assertTrue(len(BaseModel.__doc__) >= 20, "Short or no documentation")
+        self.assertTrue(len(BaseModel.save.__doc__) >= 20, "Short doc")
+        self.assertTrue(len(BaseModel.to_dict.__doc__) >= 20, "Short doc")
+        self.assertTrue(len(BaseModel.__str__.__doc__) >= 20, "Short doc")
+        self.assertTrue(len(BaseModel.__init__.__doc__) >= 20, "Short doc")
+    def test_updates_as(self):
+        base4 = BaseModel()
+        var1 = base4.updated_at
+        time.sleep(1)
+        base4.save()
+        var2 = base4.updated_at
+        self.assertNotEqual(var1, var2)
+
+class TestBaseModel_save_method(unittest.TestCase):
+    def test_save_updates_file(self):
+        bm = BaseModel()
+        bm.save()
+        bmid = "BaseModel." + bm.id
+        with open("objects_file.json", "r") as f:
+            self.assertIn(bmid, f.read())
+
 
 if __name__ == '__main__':
     unittest.main()
